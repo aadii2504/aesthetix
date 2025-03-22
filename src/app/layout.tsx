c3e3e9d66ceed5f3bac2ec01957a096ce59b69b3
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-23 00:28:15 +0530

    Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,26 +16,27 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Aesthetix",
-  description: "Aesthetix is your cutting-edge AI-powered image generation SaaS platform that brings creativity to life",
+  description:
+    "Aesthetix is your cutting-edge AI-powered image generation SaaS platform that brings creativity to life",
   metadataBase: new URL("https://Aesthetix.com"),
   openGraph: {
-  title: "Aesthetix - Free AI Image Generation",
-  description: "Turn your text into images with our free AI-powered tool",
-  images: [
-    {
-      url: "/og-image.jpg",
-      width: 1200,
-      height: 630,
-      alt: "Aesthetix AI Image Generator",
-     },
-   ],
- },
- twitter: {
-  card: "summary_large_image",
-  title: "StableMax - Free AI Image Generation",
-  description: "Turn your text into images with our free AI-powered tool",
-  images: ["/twitter-image.jpg"],
- },
+    title: "Aesthetix - Free AI Image Generation",
+    description: "Turn your text into images with our free AI-powered tool",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Aesthetix AI Image Generator",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "StableMax - Free AI Image Generation",
+    description: "Turn your text into images with our free AI-powered tool",
+    images: ["/twitter-image.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -47,12 +49,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-          >
-            {children}
-          </ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <Header/>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full h-[60px] bg-black border-b border-white p-3'>
+      
+    </div>
+  )
+}
