39d53f070f452393890ce634da019b3d72c081b7
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-26 19:40:46 +0530

    Minimal Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      <div className="">
+      <div className="flex justify-center items-center flex-col">
         <motion.h1
           initial={{
             opacity: 0,
@@ -21,6 +21,20 @@ export default function Home() {
         >
           Aesthetix
         </motion.h1>
+        <motion.p
+        initial={{
+          opacity: 0,
+          scale: 0.95,
+          filter:"blur(10px)",
+        }}
+        animate={{
+          opacity:1,
+          scale:1,
+          filter:"blur(0px)",
+        }}
+        transition={{duration:0.5, delay:0.35}} className="text-center text-white/50">
+        Turn your text into images with our free AI-powered too
+        </motion.p>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 export default function Header() {
   return (
-    <div className=' fixed top-0 w-full h-[60px] bg-black border-b border-white/60  p-3 flex justify-between items-center'>
+    <div className='fixed top-0 w-full h-[60px] bg-black border-b border-white/60  p-3 flex justify-between items-center'>
       <Link href="/"> 
       <h2 className='font-bold text-2xl'>Aesthetix</h2>
       </Link>
+      <div className="__menu">
+        <Button>Login</Button>
+      </div>
     </div>
   )
 }
