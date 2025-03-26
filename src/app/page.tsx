6f4b9e7dc1cb397487966d36bd45d18ce0bf9ae4
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-26 18:43:58 +0530

    implemented framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 "use client";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      <div className="text-4xl font-extrabold">
-
-      <h1>Aesthetix</h1>
+      <div className="">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter:"blur(10px)",
+          }}
+          animate={{
+            opacity:1,
+            scale:1,
+            filter:"blur(0px)",
+          }}
+          transition={{duration:0.5, delay:0.1}}
+          className="text-4xl sm:text-6xl font-extrabold"
+        >
+          Aesthetix
+        </motion.h1>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,6 @@ export default function Header() {
     <div className=' fixed top-0 w-full h-[60px] bg-black border-b border-white/60  p-3 flex justify-between items-center'>
       <Link href="/"> 
       <h2 className='font-bold text-2xl'>Aesthetix</h2>
-      <p>The AI Image Generator</p>
       </Link>
     </div>
   )
