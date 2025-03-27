fcb334090f0ed24cc6e7bf77465f177955f5879d
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-27 22:49:20 +0530

    Avatar Implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/Header";
+
 import Provider from "./provider";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,7 +53,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
           <Provider>
-          <Header/>
+          <Header />
           {children}
           </Provider>
         </ThemeProvider>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import Link from 'next/link'
-import React from 'react'
-import { Button } from './ui/button'
-import { signIn } from 'next-auth/react';
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { signIn, useSession } from "next-auth/react";
+import { Avatar ,AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Header() {
+  const { data: session } = useSession();
   return (
-    <div className='fixed top-0 w-full h-[60px] bg-black border-b border-white/60  p-3 flex justify-between items-center z-10'>
-      <Link href="/"> 
-      <h2 className='font-bold text-2xl'>ImagiCrea</h2>
+    <div className="fixed top-0 w-full h-[60px] bg-black border-b border-white/60  p-3 flex justify-between items-center z-10">
+      <Link href="/">
+        <h2 className="font-bold text-2xl">ImagiCrea</h2>
       </Link>
-      <div className="__menu">
-        <Button onClick={()=> signIn("google")}>Login</Button>
-      </div>
+      {!session ? (
+        <div className="__menu">
+          <Button onClick={() => signIn("google")}>Login</Button>
+        </div>
+      ) : (
+        <Avatar>
+          <AvatarImage src={session.user?.image || ""} /> 
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      )}
     </div>
-  )
+  );
 }
