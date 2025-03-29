1887a2972a5ed1b2aa019553196b7df367f380e6
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-29 18:56:02 +0530

    Wrapper Template Component

---
+++ src/app/template.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const template = ({ children }: { children: ReactNode }) => {
+  return <div className="border border-violet-500">{children}</div>;
+};
+
+export default template;
