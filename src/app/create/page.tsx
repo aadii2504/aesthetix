6efaa88ef49821de4081dbd2b6a8332fe3e12e6d
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-30 12:19:26 +0530

    Genrate Page after Form

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,12 +1,38 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-const Page = () => {
-  const [outputImg , setOutputImg] = useState<string|null>(null)
-  const [inputPrompt , setInputPrompt] = useState<string>("")
-  const [loading, setLoading] = useState<boolean>(false)
+const formSchema = z.object({
+  prompt: z.string().min(7, {message: "Prompt must  be atleast 7 characterrs long! try again :)"}),
+});
+
+export default function Page() {
+  const [outputImg, setOutputImg] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className="w-full p-3 h-dvh flex  items-center justify-start pt-[72px] flex-col">
       <div className="w-full border border-red-500 p-3">
@@ -25,17 +51,35 @@ const Page = () => {
             Drop your vibe and let the magic flow turn your words into stunning
             visuals!
           </p>
-          <div className="flex gap-2"></div>
-          <Input
-            placeholder="Cat Sitting On A Sofa....."
-            className="max-w-[70%] transition-all"
-          />
-          <Button>Generate</Button>
+          <div className="flex gap-2 w-full">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full flex gap-3 justify-center"
+              >
+                <FormField
+                  control={form.control}
+                  name="prompt"
+                  render={({ field }) => (
+                    <FormItem className=" w-full items-center flex justify-center max-w-[70%]">
+                      <FormControl>
+                        <Input
+                          placeholder="Cat Sitting On A Sofa....."
+                          className="w-full transition-all border-white"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Generate</Button>
+              </form>
+            </Form>
+          </div>
         </div>
         <div className="__output flex-[1] borders bg-white/7 rounded-lg"></div>
       </div>
     </div>
   );
-};
-
-export default Page;
+}
