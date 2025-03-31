ffe0b963bb6e43c4aee8dec59ae6aba0a82fee8d
Aadii2504 <adityadhaygude25@gmail.com>
2025-03-31 20:13:13 +0530

    Optional to inreal

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,29 +1,50 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react'
+import { Post } from "@prisma/client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
-    const [loading ,setLoading] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true);
+  const [posts, setPosts] = useState<Post[]>([]);
 
-    const fetchPosts = async ()=> {
-        try {
-            setLoading(true);
-        const response =await fetch("/api/image")
-        const data =  await response.json();
-        console.log(data);
-        } catch (error) {
-            console.error(error)
-        }finally{
-            setLoading(false)
-        }
-       };
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("/api/image");
+      const data = await response.json();
+      console.log(data);
+      setPosts(data)
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    useEffect(()=> {
-       fetchPosts();
-    },[])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return (
-    <div className='w-full min-h-dvh border border-pink-400 flex justify-center items-center p-3 pt-[72px]'>
-     {loading ? "loading" : "Page"}
+    <div className="w-full min-h-dvh border border-pink-400 flex justify-center items-center p-3 pt-[72px]">
+      {loading ? (
+        "loading"
+      ) : (
+          posts.map((post) => {
+            return (
+              <div className="w-full h-full" key={post.id}>
+                <Image
+                  alt={post.prompt}
+                  src={post.url}
+                  width={250}
+                  height={250}
+                  className="object-cover w-full h-full "
+                />
+              </div>
+            );
+          })
+      
+      )}
     </div>
-  )
+  );
 }
