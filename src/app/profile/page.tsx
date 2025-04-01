c1e12b06b614173f064b4320a6b9dec7df6409bc
Aadii2504 <adityadhaygude25@gmail.com>
2025-04-01 10:43:16 +0530

    Better Post Visibility

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Post } from "@prisma/client";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { BiLoaderCircle } from "react-icons/bi";
 
 export default function Page() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -14,7 +16,7 @@ export default function Page() {
       const response = await fetch("/api/image");
       const data = await response.json();
       console.log(data);
-      setPosts(data)
+      setPosts(data);
     } catch (error) {
       console.error(error);
     } finally {
@@ -26,24 +28,34 @@ export default function Page() {
     fetchPosts();
   }, []);
   return (
-    <div className="w-full min-h-dvh  p-3 pt-[72px] grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
+    <div className="w-full min-h-dvh  p-3 pt-[72px] grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-3">
       {loading ? (
-        "loading"
+        <div className="col-span-full flex justify-center items-center">
+          <BiLoaderCircle className="animate-spin" />
+        </div>
       ) : (
-          posts.map((post) => {
+        <AnimatePresence mode="wait">
+          {posts.map((post,index) => {
             return (
-              <div className="w-full h-full" key={post.id}>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.9, filter: "blur(10px)" }}
+                animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
+                transition={{duration:0.35 ,delay: index * 0.1}}
+                className="w-full h-full border border-b-emerald-500/40 p-3 cursor-pointer"
+                key={post.id}
+              >
                 <Image
                   alt={post.prompt}
                   src={post.url}
                   width={250}
                   height={250}
-                  className="object-cover w-full h-full "
+                  className="object-contain w-full rounded-lg"
                 />
-              </div>
+                <p className="text-white/70 mt-2 text-center">{post.prompt}</p>
+              </motion.div>
             );
-          })
-      
+          })}
+        </AnimatePresence>
       )}
     </div>
   );
